b2ce3659ab1a96a1d32ef5ba2bd874e44090babf
hibeenovul <hibeenovul19@gmail.com>
2025-06-30 14:16:44 +0100

    creating django project, vite plus react and the file project structure

---
+++ inventory_system/backend/frontend-src/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 style={{ color: "green", fontSize: "2rem" }}>🚀 Hello Ibrahim!</h1>
+        <p>This is your React app served through Django!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
